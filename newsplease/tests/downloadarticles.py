be50203557eea61209e4da5a8a1618980c76d60a
Felix Hamborg <felix@hamborg.eu>
2017-05-20 17:00:53 +0200

    add script to download and save articles

---
+++ newsplease/tests/downloadarticles.py
@@ -0,0 +1,15 @@
+import json
+
+import os
+
+name = 'trump-in-saudi-arabia.txt'
+basepath = '/Users/felix/Downloads/'
+download_dir = basepath + 'dir' + name + '/'
+os.makedirs(download_dir)
+
+articles = NewsPlease.download_from_file(basepath + name)
+
+for url in articles:
+    article = articles[url]
+    with open(download_dir + article['filename'], 'w') as outfile:
+        json.dump(article, outfile)
