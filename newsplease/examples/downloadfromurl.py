f61fdf16c7013d528559c15b3316544eba3d4030
Felix Hamborg <felix@hamborg.eu>
2017-05-21 12:38:14 +0200

    add scripts to download and save articles

---
+++ newsplease/examples/downloadfromurl.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+This script downloads article information of one URL. The results are stored in JSON-files in a sub-folder.
+You need to adapt the variables url and basepath in order to use the script.
+"""
+
+import json
+
+from newsplease import NewsPlease
+
+url = 'https://www.rt.com/news/203203-ukraine-russia-troops-border/'
+basepath = '/Users/felix/Downloads/'
+
+article = NewsPlease.download_article(url)
+
+with open(basepath + article['filename'] + '.json', 'w') as outfile:
+    json.dump(article, outfile, indent=4, sort_keys=True)
