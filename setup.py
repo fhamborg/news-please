2dfd9dceb14ad67464b7dfe40d2201f9d374ad20
Felix Hamborg <felix@hamborg.eu>
2017-08-07 17:39:14 +0900

    fix #35 version increase

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='news-please',
-      version='1.1.51',
+      version='1.1.52',
       description="news-please is an open source easy-to-use news extractor that just works.",
       long_description="""\
 news-please is an open source, easy-to-use news crawler that extracts structured information from almost any news website. It can follow recursively internal hyperlinks and read RSS feeds to fetch both most recent and also old, archived articles. You only need to provide the root URL of the news website.""",
@@ -50,7 +50,7 @@ news-please is an open source, easy-to-use news crawler that extracts structured
           'warcio>=1.3.3',
           'ago>=0.0.9',
           'six>=1.10.0',
-          'lxml>=3.35'
+          'lxml>=3.3.5'
       ],
       extras_require={
           ':python_version == "2.7"': [
