6e1016e92a177194da687f1c9191d6132aacf444
默奕 <moyi.dang@gmail.com>
2020-04-27 13:13:50 -0700

    revisions to adding commoncrawl warc date filter

--- newsplease/examples/commoncrawl.py
+++ newsplease/examples/commoncrawl.py
@@ -36,17 +36,17 @@ __credits__ = ["Sebastian Nagel"]
 
 ############ YOUR CONFIG ############
 # download dir for warc files
-my_local_download_dir_warc = './cc_download_warc_test/'
+my_local_download_dir_warc = './cc_download_warc/'
 # download dir for articles
-my_local_download_dir_article = './cc_download_articles_march_april_2020_test/'
+my_local_download_dir_article = './cc_download_articles/'
 # hosts (if None or empty list, any host is OK)
 my_filter_valid_hosts = []  # example: ['elrancaguino.cl']
 # start date (if None, any date is OK as start date), as datetime
-my_filter_start_date = datetime.datetime(2020, 3, 1)  # datetime.datetime(2016, 1, 1)
+my_filter_start_date = None  # datetime.datetime(2016, 1, 1)
 # end date (if None, any date is OK as end date), as datetime
-my_filter_end_date = datetime.datetime(2020, 4, 24)  # datetime.datetime(2016, 12, 31)
+my_filter_end_date = None  # datetime.datetime(2016, 12, 31)
 # if date filtering is strict and news-please could not detect the date of an article, the article will be discarded
-my_warc_files_start_date = datetime.datetime(2020, 3, 1)
+my_warc_files_start_date = None # example: datetime.datetime(2020, 3, 1)
 my_filter_strict_date = True
 # if True, the script checks whether a file has been downloaded already and uses that file instead of downloading
 # again. Note that there is no check whether the file has been downloaded completely or is valid!
@@ -54,13 +54,13 @@ my_reuse_previously_downloaded_files = True
 # continue after error
 my_continue_after_error = True
 # show the progress of downloading the WARC files
-my_show_download_progress = True
+my_show_download_progress = False
 # log_level
 my_log_level = logging.INFO
 # json export style
 my_json_export_style = 1  # 0 (minimize), 1 (pretty)
 # number of extraction processes
-my_number_of_extraction_processes = 16
+my_number_of_extraction_processes = 1
 # if True, the WARC file will be deleted after all articles have been extracted from it
 my_delete_warc_after_extraction = True
 # if True, will continue extraction from the latest fully downloaded but not fully extracted WARC files and then
@@ -128,15 +128,6 @@ def callback_on_warc_completed(warc_path, counter_article_passed, counter_articl
     :return:
     """
     pass
-
-def iterate_by_month(start_date, end_date, month_step=1):
-    current_date = start_date
-    while current_date < end_date:
-        yield current_date
-        carry, new_month = divmod(current_date.month - 1 + month_step, 12)
-        new_month += 1
-        current_date = current_date.replace(year=current_date.year + carry,
-                                            month=new_month)
 
 
 def main():
