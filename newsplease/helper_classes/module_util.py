2d23efb493b1bff8c1b9b799f50a80a465dcfbb7
Thihara Neranjya <thihara@Thiharas-MacBook-Pro.local>
2020-04-28 14:04:31 +0530

    Removed f-strings and used format method. Added the new config parameter into config_lib.cfg file

--- newsplease/helper_classes/module_util.py
+++ newsplease/helper_classes/module_util.py
@@ -5,7 +5,7 @@ class ClassLoader:
 	@classmethod
 	def from_string(cls, class_name):
 		if "." not in class_name:
-			raise ImportError(f"{class_name} does't look like a module path")
+			raise ImportError("{0} does't look like a module path".format(class_name))
 
 		module_name = ".".join(class_name.split(".")[:-1])
 		class_name = class_name.split(".")[-1]
@@ -14,7 +14,7 @@ class ClassLoader:
 			loaded_module = importlib.import_module(module_name)
 			loaded_class = getattr(loaded_module, class_name)
 		except Exception as e:
-			raise ImportError(
-				f"Module {module_name} does not exist or does not define a class named {class_name}") from e
+			raise ImportError("Module {0} does not exist or does not define a class named {1}".format(loaded_module,
+			                                                                                          class_name)) from e
 
 		return loaded_class
